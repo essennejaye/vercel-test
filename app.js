964c63e9b64279a72f5f2bd6472c0f80a0defa7b
essennejaye <sjeffers02@gmail.com>
2022-08-27 15:59:12 -0700

    testing

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.use(express.static('public'))
-app.get('/api/test-endpoint', (req,res) => {
-    res.status(200).json({id: 1, text: 'Test Endpoint Description One'})
-})
+// app.use(express.static('public'))
+app.get('/api/test-endpoint', (req, res) => {
+  res.status(200).json({ id: 1, text: 'Test Endpoint Description One' });
+});
 
 app.listen(port, () => {
-    console.log(`Listening, visit http://localhost:${port}`)
-})
+  console.log(`Listening, visit http://localhost:${port}`);
+});
 
-module.exports = app
+module.exports = app;
